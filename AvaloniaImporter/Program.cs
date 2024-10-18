518d21a19da460edf017757ef18de42d23c66eb8
Ethan Evans <ethanalexevans@gmail.com>
2024-10-18 14:47:11 -0700

    Modify AvaloniImporter to include all of the icons, including the filled variants

--- AvaloniaImporter/Program.cs
+++ AvaloniaImporter/Program.cs
@@ -43,37 +43,39 @@ namespace AvaloniaImporter
             var curDir = Directory.GetCurrentDirectory();
             var absSvgPath = Path.GetFullPath(Path.Combine(curDir, "../assets"));
 
-            File.WriteAllText("FluentUiIcons_Regular.xaml", GenerateAvaloniaFluentIcons(absSvgPath, "regular"));
-
-            File.WriteAllText("FluentUiIcons_Filled.xaml", GenerateAvaloniaFluentIcons(absSvgPath, "filled"));
-            
-            File.WriteAllText("icons_avalonia.html", GenerateAvaloniaFluentIcons(absSvgPath, "regular", true));
+            Console.WriteLine($"Using assets from: {absSvgPath}");
+            File.WriteAllText("icons_avalonia.html", GenerateAvaloniaFluentIcons(absSvgPath));
 
         }
 
-        static string GenerateAvaloniaFluentIcons(string absSvgPath, string designator, bool markdownMode = false)
+        static string GenerateAvaloniaFluentIcons(string absSvgPath)
         {
             var svgPaths = Directory.EnumerateFiles(absSvgPath, "*.svg", new EnumerationOptions() { RecurseSubdirectories = true })
-            .Where(x => x.Contains("_regular"))
-            .ToList();
+                .ToList();
 
             var entries = new Dictionary<string, List<string>>();
             var highestResVersion = new Dictionary<string, string>();
             var iconPaths = new Dictionary<string, (string path, string streamgeoxaml, string streamgeoraw)>();
 
-            foreach (var entry in svgPaths.Select(x => (x.Replace(absSvgPath, string.Empty).Split('/')[1], x)))
-            {
-                if (!entries.ContainsKey(entry.Item1))
-                    entries.Add(entry.Item1, new List<string>());
+            var entryNames = svgPaths.Select(x => (x.Replace(absSvgPath, string.Empty).Split(Path.DirectorySeparatorChar.ToString())[1], x));
 
-                entries[entry.Item1].Add(entry.x);
+            foreach (var entry in entryNames) {
+                var designator = entry.Item2.Split('_').Last().Replace(".svg", string.Empty);
+                var designatedKey = entry.Item1 + $" {designator}";
+
+                if (!entries.ContainsKey(designatedKey)) {
+                    entries.Add(designatedKey, new List<string>());
+                }
+
+                entries[designatedKey].Add(entry.x);
             }
 
+            Console.WriteLine($"Found {entries.Count} unique icons");
 
-            foreach (var entry in entries.Select(x => (x.Key, x.Value.OrderByDescending(x => x).First())))
-            {
-                if (!highestResVersion.ContainsKey(entry.Item1))
+            foreach (var entry in entries.Select(x => (x.Key, x.Value.OrderByDescending(x => x).First()))) {
+                if (!highestResVersion.ContainsKey(entry.Item1)) {
                     highestResVersion.Add(entry.Item1, string.Empty);
+                }
 
                 highestResVersion[entry.Item1] = entry.Item2;
             }
@@ -82,6 +84,8 @@ namespace AvaloniaImporter
             foreach (var entry in highestResVersion)
             {
                 var path = entry.Value;
+                var fileName = Path.GetFileNameWithoutExtension(path);
+                var designator = fileName.Split('_').Last();
 
                 var xmlDoc = new XmlDocument(); // Create an XML document object    
                 var xmlStr = File.ReadAllText(path);
@@ -92,80 +96,29 @@ namespace AvaloniaImporter
 
                 // Get elements
                 var svgNode = xmlDoc.SelectSingleNode("/svg");
-
-
-                var key = $"{entry.Key.Replace(' ', '_').ToLowerInvariant()}_{designator.ToLowerInvariant()}";
+                var key = $"{entry.Key.Replace(' ', '_').ToLowerInvariant()}";
 
                 var pathNodes = xmlDoc.SelectNodes("//path");
-
                 var pathAccumulator = "";
 
-                foreach (var pathNode in pathNodes.Cast<XmlNode>())
-                {
+                foreach (var pathNode in pathNodes.Cast<XmlNode>()) {
                     var pathData = pathNode.Attributes["d"].Value + " ";
                     pathAccumulator += pathData;
                 }
                 
                 var finalDG = $@"<StreamGeometry x:Key=""{key}"">{pathAccumulator.Trim()}</StreamGeometry>";
-
                 iconPaths.Add(key, (Path.GetRelativePath(absSvgPath + "/../", entry.Value), finalDG, pathAccumulator.Trim()));
             }
 
 
-            if (markdownMode)
-            {
-                var outMarkdown = new StringBuilder("");
-//
-//                 var header = $@"---
-// Title: Icons
-// Order: 90
-// ---
-//
-// | Key  | Icon     | Code  |
-// | :---  | :----:   |  :--- |";                
-//                 
-//                 outMarkdown.AppendLine(header);
-
-                foreach (var v in iconPaths.Select(x => x)) // &lt; &gt;
-                {
-                   // var h = $@"| {v.Key} | <img src='https://raw.githubusercontent.com/microsoft/fluentui-system-icons/master/{v.Value.path}' width='24' height='24'> |  <pre style='max-width:250px;'><code class='language-xml hljs'> {v.Value.streamgeoxaml.Replace("<","&lt;").Replace(">","&gt;")} </code></pre> |";
-                   var h =
-                       $@"<p style=""display: inline-flex;""><img src=""https://raw.githubusercontent.com/microsoft/fluentui-system-icons/master/{v.Value.path}"" width=""24"" height=""24"">{v.Key}<div class=""language-xml highlighter-rouge"" style=""max-width: 800px;""> <div class=""highlight""> <pre class=""highlight""><code><span class=""nt"">&lt;StreamGeometry</span> <span class=""na"">x:Key=</span><span class=""s"">&quot;{v.Key}&quot;</span><span class=""nt"">&gt;</span>{v.Value.streamgeoraw}<span class=""nt"">&lt;/StreamGeometry&gt;</span></code></pre> </div> </div> </p>";
-                    outMarkdown.AppendLine(h);
-                }
-
-                return outMarkdown.ToString();
+            var outHtml = new StringBuilder("");
+            foreach (var v in iconPaths.Select(x => x)) {
+               var h =
+                   $@"<p style=""display: inline-flex;""><img src=""https://raw.githubusercontent.com/AvaloniaUI/fluentui-system-icons/master/{v.Value.path}"" width=""24"" height=""24"">{v.Key}<div class=""language-xml highlighter-rouge"" style=""max-width: 800px;""> <div class=""highlight""> <pre class=""highlight""><code><span class=""nt"">&lt;StreamGeometry</span> <span class=""na"">x:Key=</span><span class=""s"">&quot;{v.Key}&quot;</span><span class=""nt"">&gt;</span>{v.Value.streamgeoraw}<span class=""nt"">&lt;/StreamGeometry&gt;</span></code></pre> </div> </div> </p>";
+                outHtml.AppendLine(h);
             }
-            
-            var outXml = new StringBuilder("");
 
-            
-            outXml.AppendLine(@"<Styles xmlns=""https://github.com/avaloniaui""");
-            outXml.AppendLine(@"xmlns:x=""http://schemas.microsoft.com/winfx/2006/xaml"">");
-            outXml.AppendLine(@"<Design.PreviewWith>");
-            
-            
-
-            
-            foreach (var keyName in iconPaths.Select(x => x.Key))
-            {
-                outXml.AppendLine(@"<StackPanel Orientation=""Horizontal"">");
-                outXml.AppendLine($@"<PathIcon Data=""{{DynamicResource {keyName}}}"" />");
-                outXml.AppendLine($@"<TextBlock Margin=""10 0""  Text=""{keyName}"" />");
-                outXml.AppendLine(@"</StackPanel>");
-            }            
-            
-            outXml.AppendLine(@"</Design.PreviewWith>");
-            outXml.AppendLine(@"<Styles.Resources>");
-
-            
-            foreach (var iconPath in iconPaths.Select(x => x.Value))
-                outXml.AppendLine(iconPath.streamgeoxaml);
-
-            outXml.AppendLine(@"</Styles.Resources>");
-            outXml.AppendLine(@"</Styles>");
-
-            return PrettyXml(outXml.ToString());
+            return outHtml.ToString();
         }
     }
 }
